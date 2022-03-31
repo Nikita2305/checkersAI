b3683345296d03a331c80307aa24c11da1237fd3
Nikita Nikitin <nikitanikitin2305@gmail.com>
2022-03-31 20:53:53 +0300

    Final commit

--- checkers_engine/players.py
+++ checkers_engine/players.py
@@ -1,5 +1,9 @@
 from checkers.board import Board
 from functools import reduce
+import random
+
+def other_player_id(player):
+    return 3 - player
 
 class Player:
     
@@ -24,8 +28,8 @@ class BoardTreeNode:
         self.sons = []
 
     def process_score(self):
-        if (self.depth == 0):
-            self.score = self.estimator(self.board, self.host_id)
+        if (self.depth == 0 or not self.board.get_possible_moves()):
+            self.score = self.estimator(self.board)
             return
         for move in self.board.get_possible_moves():
             self.sons.append(BoardTreeNode(self.board.create_new_board_from_move(move), self.host_id, self.depth - 1, self.estimator, move))
@@ -57,8 +61,13 @@ class BasicMinimaxPlayer(Player):
         root = BoardTreeNode(self.board, self.player_id, self.depth, self.get_grade)
         root.process_score()
         return root.choose_best_move()
+    
+    def easy_estimation(self, board, player):
+        if (board.get_possible_moves()):
+            raise RuntimeError("Not possible")
+        return 1e9 * (1 if board.player_turn != player else -1)
         
-    def get_grade(self, board, player):
+    def get_grade(self, board):
         raise RuntimeError("Not implemented")
     
 
@@ -67,13 +76,24 @@ class EasyPlayer(BasicMinimaxPlayer):
     def __init__(self, player_id):
         super().__init__(player_id, 3, True)  
  
-    def get_grade(self, board, player):
-        return 
+    def get_grade(self, board):
+        return random.random()
         
 class HardPlayer(BasicMinimaxPlayer):
    
     def __init__(self, player_id):
         super().__init__(player_id, 2, False)
- 
-    def get_grade(self, board, player):
-        return 0
+
+    def estimate_player_pieces(self, board, player):
+        cost = 0
+        for piece in board.searcher.get_pieces_by_player(player):
+            cost += (1000 if piece.king else 5000)
+        return cost
+
+    def get_grade(self, board):
+        try:
+            return self.easy_estimation(board, self.player_id)
+        except RuntimeError:
+            pass 
+        diff = self.estimate_player_pieces(board, self.player_id) - self.estimate_player_pieces(board, other_player_id(self.player_id))
+        return diff
--- playground/competition.py
+++ playground/competition.py
@@ -3,24 +3,25 @@ import traceback
 import logging
 
 logger = logging.getLogger("")
+ITERATIONS = 5
+for i in range(ITERATIONS):
+    logger.warning(f"Starting new game.")
+    players = [None, HardPlayer(1), EasyPlayer(2)] # 1-indexation
+    player_index = 1
 
-players = [None, HardPlayer(1), EasyPlayer(2)] # 1-indexation
-player_index = 1
+    while (True):
+        current_board = players[player_index].board
+        player_index = current_board.player_turn
+        other_index = 3 - player_index
+        if (not current_board.get_possible_moves()):
+            logger.warning(f"End of game. Player{other_index} won!")
+            break
 
-while (True):
-    current_board = players[player_index].board
-    player_index = current_board.player_turn
-    other_index = 3 - player_index
-    if (not current_board.get_possible_moves()):
-        logger.warning(f"End of game. Player{ohter_index} won!")
-        quit()
-
-    try:
-        move = players[player_index].choose_move()
-        print(move)
-        players[player_index].handle_move(move)
-        players[other_index].handle_move(move)
-    except Exception as error:
-        trace = ''.join(traceback.format_list(traceback.extract_tb(error.__traceback__)))
-        logger.error(f"Move trouble. Player_index: {player_index}\n\nTrace:\n{trace}\n\nError: {error}")
-        quit()
+        try:
+            move = players[player_index].choose_move()
+            players[player_index].handle_move(move)
+            players[other_index].handle_move(move)
+        except Exception as error:
+            trace = ''.join(traceback.format_list(traceback.extract_tb(error.__traceback__)))
+            logger.error(f"Move trouble. Player_index: {player_index}\n\nTrace:\n{trace}\n\nError: {error}")
+            quit()
